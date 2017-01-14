56e2308076e0e1927b6e91d9398af95862d22e08
steppannws <steppannws@gmail.com>
2017-01-14 16:40:28 -0300

    Utils.js

---
+++ Utils.js
@@ -0,0 +1,9 @@
+function test() {
+	console.log('TEST');
+}
+
+const Utils = {
+	test: test
+}
+
+module.exports = Utils;
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 
 import { NativeModules } from 'react-native';
-
+import React from 'react';
+import Utils from './Utils';
 const { RNReactNativeZxzUtils } = NativeModules;
 
 export default RNReactNativeZxzUtils;
+export {
+	Utils
+};
